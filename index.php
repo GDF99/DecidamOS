92995ff26dcd49c988d8dc26f6798c34ddf339af
LauVM <125633448+mariposita2402@users.noreply.github.com>
2023-06-03 11:06:24 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,67 +1,50 @@
-<?php
-    include('connection.php');
-    $con = connection();
-
-    $selet_sql = "SELECT * FROM persona";
-    $query = mysqli_query($con,$selet_sql);
-
+<?php 
+    session_start(); 
+    if(isset($_SESSION['nombre'])){
+    }
 ?>
-<!DOCTYPE html>
-<html lang="es">
+  <!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DecidamOS</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="assest/css/index.css">
+    <link rel="icon" type="image/png" href="assest/img/logo.png">
+    <title>Login</title>
+   
 </head>
 <body>
-    <div class="input-group justify-content-center alig-items-center">
-        <form action="create_persona.php" method="POST"><!-- metodo para crear -->
-            <h1>Crear Persona</h1>
-
-            <input type="text" name="1er_Nombre" placeholder="1er_Nombre"  class="form-control" >
-            <input type="text" name="2do_Nombre" placeholder="2do_Nombre"  class="form-control">
-            <input type="text" name="1er_Apellido" placeholder="1er_Apellido"  class="form-control">
-            <input type="text" name="2do_Apellido" placeholder="2do_Apellido"  class="form-control">
-            <input type="number" name="CI" placeholder="CI" class="form-control">
-            <div class="row w-100 align-items-center">
-    			<div class="col text-center">
-                <input type="submit" value="Crear" class="btn btn-warning regular-button">
-    			</div>	
-    		</div>
-        </form>
-    </div>
-    <div>
-        <h2>Personas</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Apellidos</th>
-                    <th>CI</th>
-
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while($row = mysqli_fetch_array($query)): ?>
-                <tr>
-                <th><?=$row['ID']?></th>
-                <th><?=$row['1er_Nombre'].' '.$row['2do_Nombre'];?></th>
-                <th><?=$row['1er_Apellido'].' '.$row['2do_Apellido'];?></th>
-                <th><?=$row['CI']?></th>
-
-                <th><a href="">Editar</a></th>
-                <th><a href="">Eliminar</a></th>
-                </tr>
-                <?php endwhile;?>
-            </tbody>
-        </table>
-    </div>
+    <div class="contenedor">
+     <div class="contenedorcentrado">
+         <div class="login">
+                 <div class="titulo">
+                    BIENVENIDO
+                 </div>
+                 <hr>
+                 <form action="controller/validacion.php" method="POST">
+                     <div class="field email">
+                        <div class="input-area">
+                            <input id="nombre" type="text" name="email" placeholder="Usuario o correo" required>
+                         </div>
+                    </div>
+                    <div class="field password">
+                         <div class="input-area">
+                            <input id="password" type="password" placeholder="Contraseña" name="password">
+                         </div>
+                         <div class="error error-txt">Credenciales incorrectas</div>
+                    </div>
+                         <button type="submit" title="Ingresar" name="Ingresar">Acceder</button>
+                        </form>
+         </div>  
+         <div id="derecho">
+         </div>
+        </div>
+ </div>
+<script src="assest/js/scriptlo.js"></script>
 
 </body>
+
+
 </html>
